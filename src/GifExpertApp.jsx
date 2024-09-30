b27b59c706650a23ff0a3bec6970586a891d0de5
PORTELA PAZ Hipolito <hipolito.portela@soprasteria.com>
2024-09-30 11:25:54 +0200

    First commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { AddCategory, GifGrid } from "./components";
+
+export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["arale"]);
+  const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
+    setCategories([...categories, newCategory]);
+  };
+  return (
+    <>
+      <h1>GiftExpertApp</h1>
+
+      <AddCategory onNewCategory={(event) => onAddCategory(event)} />
+
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
+    </>
+  );
+};
---
+++ src/components/GifGrid.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import { getGifs } from "../helpers/getGifs";
+import { GifItem } from "./GifItem";
+import { useFetchGif } from "../hooks/useFetchGif";
+
+export const GifGrid = ({ category }) => {
+
+  const {images, isLoading} = useFetchGif(category);
+
+  //console.log({images, isLoading});
+
+  return (
+    <>
+      <h3>{category}</h3>
+      {
+        isLoading && (<h2>Cargando...</h2>)
+      }
+
+      <div className="card-grid">
+        {
+            images.map((image) => (<GifItem key={image.id} {...image} />))
+        }
+      </div>
+    </>
+  );
+};
